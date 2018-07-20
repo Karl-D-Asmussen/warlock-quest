6ddc642cc9cbb52f1ca19020ced883a8c73df5a1
Karl D. Asmussen <karl.d.asmussen@gmail.com>
2018-07-21 00:15:21 +0200

    Sat Jul 21 00:15:21 CEST 2018

--- rolls.rb
+++ rolls.rb
@@ -5,7 +5,7 @@ class Die
   def self.seed=(s) @@seed = s end
   
   attr_reader :seed, :sides, :generator
-  def initialise(sides)
+  def initialize(sides)
     @sides = sides
     @seed = @@seed
     @generator = nil
@@ -19,12 +19,14 @@ class Die
     @generator ||= Random.new(@@seed)
 
     case
-    when n > 1 then n.times.map { @generator.rand(1..@sides) }.sort.reverse
+    when n > 1 then n.times.map { @generator.rand(1..@sides) }
     when n == 1 then @generator.rand(1..@sides)
     else raise
     end
   end
 end
 
-d4, d6, d8, d10, d12, d20, d100 = [4, 6, 8, 10, 12, 20, 100].map(&Die.method(:new))
+class Array; def tros; sort { |a, b| b <=> a } end; end
+
+Dice = [4,6,8,10,12,20,100].map(&Die.method(:new))
 
