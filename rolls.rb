0d78717b8a0b848f5d8af5c0b6161e9de1aea702
Karl D. Asmussen <karl.d.asmussen@gmail.com>
2018-07-21 13:22:24 +0200

    Sat Jul 21 13:22:24 CEST 2018

--- rolls.rb
+++ rolls.rb
@@ -11,6 +11,7 @@ class Die
     @generator = nil
   end
 
+
   def roll(n=1)
     raise unless n.is_a? Integer
     raise if n < 1
@@ -25,18 +26,19 @@ class Die
     end
   end
 
-  def +(b) roll + b end
-  def -(b) roll - b end
+  def to_int; roll end
+  def +(b) roll + b.to_int end
+  def -(b) roll - b.to_int end
 end
 
 class Array; def tros; sort { |a, b| b <=> a } end; end
 
-Dice = [4,6,8,10,12,20,100].map { |n| [ n, Die.new n ] }.to_h.freeze
+Dice = [4,6,8,10,12,20,100].map { |n| [ n, Die.new(n)] }.to_h.freeze
 
-def d4; Dice[4] end
-def d6; Dice[6] end
-def d8; Dice[8] end
-def d10; Dice[10] end
-def d12; Dice[12] end
-def d20; Dice[20] end
-def d100; Dice[100] end
+def   d4(n=nil); if n.nil? then Dice[4]   else Dice[4].roll(n)   end end
+def   d6(n=nil); if n.nil? then Dice[6]   else Dice[6].roll(n)   end end
+def   d8(n=nil); if n.nil? then Dice[8]   else Dice[8].roll(n)   end end
+def  d10(n=nil); if n.nil? then Dice[10]  else Dice[10].roll(n)  end end
+def  d12(n=nil); if n.nil? then Dice[12]  else Dice[12].roll(n)  end end
+def  d20(n=nil); if n.nil? then Dice[20]  else Dice[20].roll(n)  end end
+def d100(n=nil); if n.nil? then Dice[100] else Dice[100].roll(n) end end
